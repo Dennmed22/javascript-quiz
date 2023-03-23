14af6dc26f6e47ce3390f9750480908702b3fe85
Dennmed22 <medrano.dennis@yahoo.com>
2023-03-22 20:16:15 -0400

    changes to html and js

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # javascript-quiz 
 
 # First Commit
+
--- script.js
+++ script.js
@@ -3,23 +3,35 @@ const timeCounter = document.querySelector("#counter");
 const choicesPicked = document.querySelector("#options");
 const submitButton = document.querySelector("#submit-score");
 const startButton = document.querySelector("#start-button");
-const userName = document.querySelector("#name");
-const feedbackWr = document.querySelector("#feedback");
-const indexCurrentQuestion = 0;
-const time = questions.length * 15;
-let timerId;
+
 
 const questioning = [
     {
         prompt: "Commonly used data types DO Not Include?",
         options: ["Strings", "Booleans", "Alerts", "Numbers"],
         answer: "Alerts"
+    },
+
+    {
+        prompt: "The condition in an if / else statement is enclosed with ______.",
+        options: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+        answer: "Curly Brackets"
+    },
+
+    {
+        prompt: "Arrays in JavaScript can be used to store ______.",
+        options: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer: "All of the Above"
+    },
+
+    {
+        prompt: "String values must be enclosed within _____ when being assigned to variables.",
+        options: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        answer: "Quotes"
+    },
+
+    {
+        prompt: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
+        answer: "Console.log"
     },]
-
-    console.log(questioning);
-
-   /* {
-        prompt: "Commonly used data types DO Not Include?",
-        options: ["<Strings>", "<Booleans>", "<Alerts>", "<Numbers>"],
-        answer: "<Alerts>"
-    }*/
